1cb0a688c739147b7c67de642db0214483b7f1e7
Harshal Dhake <harshaldhake123@gmail.com>
2020-08-02 14:13:30 +0530

    Level 1: Register users with email, password

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const app = express();
+const mongoose = require("mongoose");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const userSchema = {
+    email: String,
+    password: String
+};
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", function (req, res) {
     res.render("home");
@@ -17,6 +26,34 @@ app.get("/login", function (req, res) {
 
 app.get("/register", function (req, res) {
     res.render("register");
+});
+
+app.post("/register", function (req, res) {
+    const newUser = new User({
+        email: req.body.email,
+        password: req.body.password
+    });
+    newUser.save(function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("secrets");
+        }
+    });
+});
+
+app.post("/login", function (req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
+    User.findOne({email: email}, function (err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser.password === password) {
+                res.render("secrets");
+            }
+        }
+    });
 });
 
 app.listen(3000, function () {
